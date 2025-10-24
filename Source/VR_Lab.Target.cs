e72da2db5a10e9f1fc261ad2662e2c5922f00c0c
Corysia Taware <corysia.taware@gmail.com>
2025-10-24 14:20:52 -0700

    Upgrade to UE 5.6

--- Source/VR_Lab.Target.cs
+++ Source/VR_Lab.Target.cs
@@ -9,7 +9,7 @@ public class VR_LabTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("VR_Lab");
 	}
 }
--- Source/VR_LabEditor.Target.cs
+++ Source/VR_LabEditor.Target.cs
@@ -9,7 +9,7 @@ public class VR_LabEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("VR_Lab");
 	}
 }
